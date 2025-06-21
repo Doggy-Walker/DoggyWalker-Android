74800da453ee27bc9b2e55e51f387980f7550e47
sonms <smsms5676@naver.com>
2025-06-21 15:26:03 +0900

    setting/#1: 디자인 시스템 세팅

--- app/src/main/java/com/doggy/walker/MainActivity.kt
+++ app/src/main/java/com/doggy/walker/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.doggy.walker.ui.theme.DoggyWalkerTheme
+import com.doggy.walker.core.designsystem.theme.DoggyWalkerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
